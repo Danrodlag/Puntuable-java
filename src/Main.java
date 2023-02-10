6462a7d0238f3d89453c0ada9f35df4092d08d25
Daniel <drodenaslagares@gmail.com>
2023-02-10 14:22:07 +0100

    Menú listo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    protected Scanner teclado = new Scanner(System.in);
+    public static void main(String[] args){
+        String entrada = "3";
+        while (!entrada.equals("9")){
+            System.out.print("""
+                    \t1. Añadir una entrada al atlas    2. Mostrar el contenido actual del atlas
+                    \t3. Buscar una entrada del atlas   4. Modificar una entrada del atlas
+                    \t5. Ordenar por nombre de país     6. Mostrar el contenido del atlas con iterador
+                    \t7. Eliminar una entrada del atlas 8. Eliminar el contenido completo del atlas
+                    \t9. Salir
+                    \tTeclea una de las opciones:\s""");
+            // Aquí hay que implementar la captura de errores al meter un caracter
+            Opciones eleccion = new Opciones();
+            entrada = eleccion.teclado.next().trim();
+
+            eleccion.elegirOpcion(entrada);
+
+
+        }
     }
 }
---
+++ src/Opciones.java
@@ -0,0 +1,37 @@
+public class Opciones extends Main{
+
+    protected void elegirOpcion(String entrada){
+
+        switch (entrada){
+            case "1":
+                System.out.println("he");
+                break;
+            case "2":
+                System.out.println("he");
+                break;
+            case "3":
+                System.out.println("he");
+                break;
+            case "4":
+                System.out.println("he");
+                break;
+            case "5":
+                System.out.println("he");
+                break;
+            case "6":
+                System.out.println("he");
+                break;
+            case "7":
+                System.out.println("he");
+                break;
+            case "8":
+                System.out.println("he");
+                break;
+            case "9":
+                System.out.println("Talue");
+                break;
+            default:
+                System.out.println("Introduzca numero valido");
+        }
+    }
+}
